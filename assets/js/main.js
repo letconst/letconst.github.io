8f0bb322387839215b7814a41f28397bcab15f16
letconst <2000takeru93@gmail.com>
2023-03-16 01:53:56 +0900

    feat: 下線付きヘッダーが画面内に入った際にアニメーションするように+α

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,31 @@ const addBlankTargetToExternalLinks = () => {
     }
 };
 
+/**
+ * 画面内に表示された下線付きヘッダーにアニメーションclassを付与する
+ * @param {HTMLElement} targetElem アニメーションclassを付与する対象の要素
+ */
+const headerAnimationOnInView = (targetElem) => {
+    const windowHeight = window.innerHeight;
+    const elemTop      = targetElem.getBoundingClientRect().top;
+    const elemBottom   = elemTop + targetElem.offsetHeight;
+
+    const isTargetInView = (elemTop <= windowHeight) && (elemBottom >= 0);
+
+    if (isTargetInView) {
+        targetElem.classList.add('js-animated');
+    }
+};
+
 document.addEventListener('DOMContentLoaded', () => {
     addBlankTargetToExternalLinks();
+
+    const underlineHeaders = document.querySelectorAll('.main-content h2');
+
+    // 下線付きヘッダーのアニメーショントリガー
+    window.addEventListener('scroll', () => {
+        underlineHeaders.forEach((elem) => {
+            headerAnimationOnInView(elem);
+        });
+    });
 });
