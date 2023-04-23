74d167bc4e9f39de319d41ff577af835854e87fa
letconst <2000takeru93@gmail.com>
2023-04-23 15:15:43 +0900

    improve: jsの即時関数化および一部処理最適化

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,45 @@
-/**
- * ページ上のすべての外部リンクに「target="_blank"」属性を設定する
- */
-const addBlankTargetToExternalLinks = () => {
-    const externalLinks = document.querySelectorAll(`a[href^="http"]:not([href*="${ location.hostname }"])`);
+(() => {
+    /**
+    * ページ上のすべての外部リンクに「target="_blank"」属性を設定する
+    */
+    const addBlankTargetToExternalLinks = () => {
+        const externalLinks = document.querySelectorAll(`a[href^="http"]:not([href*="${ location.hostname }"])`);
 
-    for (const link of externalLinks) {
-        link.setAttribute('target', '_blank');
-    }
-};
+        for (const link of externalLinks) {
+            link.setAttribute('target', '_blank');
+        }
+    };
 
-/**
- * 画面内に表示された下線付きヘッダーにアニメーションclassを付与する
- * @param {HTMLElement} targetElem アニメーションclassを付与する対象の要素
- */
-const headerAnimationOnInView = (targetElem) => {
-    const windowHeight = window.innerHeight;
-    const elemTop      = targetElem.getBoundingClientRect().top;
-    const elemBottom   = elemTop + targetElem.offsetHeight;
+    /**
+    * 画面内に表示された下線付きヘッダーにアニメーションclassを付与する
+    * @param {HTMLElement} targetElem アニメーションclassを付与する対象の要素
+    */
+    const headerAnimationOnInView = (targetElem) => {
+        const windowHeight = window.innerHeight;
+        const elemTop      = targetElem.getBoundingClientRect().top;
+        const elemBottom   = elemTop + targetElem.offsetHeight;
 
-    const isTargetInView = (elemTop <= windowHeight) && (elemBottom >= 0);
+        const isTargetInView = (elemTop <= windowHeight) && (elemBottom >= 0);
 
-    if (isTargetInView) {
-        targetElem.classList.add('js-animated');
-    }
-};
+        if (isTargetInView) {
+            targetElem.classList.add('js-animated');
+        }
+    };
 
-document.addEventListener('DOMContentLoaded', () => {
-    addBlankTargetToExternalLinks();
+    document.addEventListener('DOMContentLoaded', () => {
+        addBlankTargetToExternalLinks();
 
-    const underlineHeaders = document.querySelectorAll('.main-content h2');
+        const underlineHeaders = document.querySelectorAll('.main-content h2');
 
-    // 下線付きヘッダーのアニメーショントリガー
-    window.addEventListener('scroll', () => {
-        underlineHeaders.forEach((elem) => {
-            headerAnimationOnInView(elem);
+        // 下線付きヘッダーのアニメーショントリガー
+        window.addEventListener('scroll', () => {
+            underlineHeaders.forEach((elem) => {
+                // 適用済みなら処理しない
+                if (elem.classList.contains('js-animated'))
+                    return;
+
+                headerAnimationOnInView(elem);
+            });
         });
     });
-});
+})();
